7c7e54da76c177388fb80ecfd895dc3a1fc60580
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-11-11 16:41:45 +0700

    fix(ui): move eye icon outside `AppTextField` to avoid unintended focus

--- modules/ui/lib/src/widgets/inputs/text_field.dart
+++ modules/ui/lib/src/widgets/inputs/text_field.dart
@@ -1,8 +1,8 @@
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:ui/src/themes/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:remixicon/remixicon.dart';
 
 class AppTextField extends HookWidget {
   const AppTextField({
@@ -112,61 +112,65 @@ class AppTextField extends HookWidget {
             borderRadius: BorderRadius.circular(context.appBorders.borderRadiusMd),
             boxShadow: effectiveBoxShadow,
           ),
-          child: TextField(
-            controller: controller,
-            focusNode: focusNode,
-            enabled: !disabled,
-            readOnly: readOnly,
-            autofocus: autoFocus,
-            autocorrect: autoCorrect,
-            enableSuggestions: enableSuggestions,
-            obscureText: textObscure.value,
-            scrollPadding: scrollPadding ?? EdgeInsets.zero,
-            maxLength: maxLength,
-            textCapitalization: textCapitalization ?? TextCapitalization.none,
-            textInputAction: textInputAction,
-            keyboardType: keyboardType,
-            decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: hintText,
-              labelText: labelText,
-              floatingLabelBehavior: FloatingLabelBehavior.auto,
-              floatingLabelStyle: context.appTypography.regular.textDefault.copyWith(
-                color: effectiveFloatingLabelTextColor,
+          child: Stack(
+            alignment: Alignment.centerRight,
+            children: [
+              TextField(
+                controller: controller,
+                focusNode: focusNode,
+                enabled: !disabled,
+                readOnly: readOnly,
+                autofocus: autoFocus,
+                autocorrect: autoCorrect,
+                enableSuggestions: enableSuggestions,
+                obscureText: textObscure.value,
+                scrollPadding: scrollPadding ?? EdgeInsets.zero,
+                maxLength: maxLength,
+                textCapitalization: textCapitalization ?? TextCapitalization.none,
+                textInputAction: textInputAction,
+                keyboardType: keyboardType,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: hintText,
+                  labelText: labelText,
+                  floatingLabelBehavior: FloatingLabelBehavior.auto,
+                  floatingLabelStyle: context.appTypography.regular.textDefault.copyWith(
+                    color: effectiveFloatingLabelTextColor,
+                  ),
+                  labelStyle: context.appTypography.regular.textDefault.copyWith(color: context.appColors.textSoft400),
+                  hintStyle: context.appTypography.regular.textDefault.copyWith(color: context.appColors.textSoft400),
+                  suffixIconConstraints: const BoxConstraints(minHeight: 0, minWidth: 0),
+                  suffixIcon: null,
+                ),
+                cursorColor: context.appColors.primaryBase,
+                style: context.appTypography.regular.textDefault.copyWith(color: effectiveTextColor, height: 1.h),
+                onTapOutside: (_) => focusNode.unfocus(),
+                onTap: onTap,
+                onChanged: onChanged,
+                onSubmitted: onSubmitted,
               ),
-              labelStyle: context.appTypography.regular.textDefault.copyWith(color: context.appColors.textSoft400),
-              hintStyle: context.appTypography.regular.textDefault.copyWith(color: context.appColors.textSoft400),
-              suffixIconConstraints: const BoxConstraints(minHeight: 0, minWidth: 0),
-              suffixIcon: textObscure.value
-                  ? GestureDetector(
-                      onTap: () => textObscure.value = !textObscure.value,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: context.appSpacing.x4s),
-                        child: Icon(
-                          textObscure.value ? Remix.eye_fill : Remix.eye_off_fill,
-                          color: context.appColors.textSoft400,
-                          size: context.appTypography.regular.text16.fontSize,
-                        ),
-                      ),
-                    )
-                  : null,
-            ),
-            cursorColor: context.appColors.primaryBase,
-            style: context.appTypography.regular.textDefault.copyWith(color: effectiveTextColor, height: 1.h),
-            onTapOutside: (_) => focusNode.unfocus(),
-            onTap: onTap,
-            onChanged: onChanged,
-            onSubmitted: onSubmitted,
+
+              if (this.textObscure)
+                GestureDetector(
+                  behavior: HitTestBehavior.translucent,
+                  onTap: () => textObscure.value = !textObscure.value,
+                  child: FaIcon(
+                    textObscure.value ? FontAwesomeIcons.eye : FontAwesomeIcons.eyeSlash,
+                    color: context.appColors.textSoft400,
+                    size: context.appTypography.regular.text16.fontSize,
+                  ),
+                ),
+            ],
           ),
         ),
         if (error) ...[
-          SizedBox(height: context.appSpacing.x5s),
+          SizedBox(height: context.appSpacing.x6s),
           Row(
             children: [
-              Icon(
-                Remix.error_warning_fill,
+              FaIcon(
+                FontAwesomeIcons.circleExclamation,
+                size: context.appTypography.regular.text10.fontSize,
                 color: context.appColors.errorBase,
-                size: context.appTypography.regular.text12.fontSize,
               ),
               SizedBox(width: context.appSpacing.x5s),
               Text(
@@ -175,7 +179,6 @@ class AppTextField extends HookWidget {
                   decorationThickness: 0,
                   decoration: TextDecoration.none,
                   color: context.appColors.errorBase,
-                  height: 1.h,
                 ),
               ),
             ],
