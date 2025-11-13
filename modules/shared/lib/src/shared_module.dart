52a5c53267467097cb7b736c561d7aa126679a1c
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-11-13 15:45:56 +0700

    feat(core): register shared dependencies using GetIt in `SharedModule`

--- modules/shared/lib/src/shared_module.dart
+++ modules/shared/lib/src/shared_module.dart
@@ -1,8 +1,46 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hive_ce/hive.dart';
+import 'package:shared/src/config/config.dart';
+import 'package:shared/src/injection/service_locator.dart';
+import 'package:shared/src/managers/session_manager.dart';
+import 'package:shared/src/models/session/session_model.dart';
 import 'package:shared/src/module/base_module.dart';
+import 'package:shared/src/network/dio_client.dart';
+import 'package:shared/src/network/dio_factory.dart';
+import 'package:shared/src/storage/hive_encryption.dart';
+import 'package:shared/src/storage/hive_manager.dart';
+import 'package:shared/src/storage/hive_storage.dart';
+import 'package:shared/src/storage/secure_storage.dart';
+import 'package:shared/src/utilities/directory_util.dart';
 
 class SharedModule extends BaseModule {
   @override
   void setupDependencies() {
-    // Setup shared dependencies here
+    sl.registerSingleton(Hive);
+
+    sl.registerLazySingleton(() => DioFactory.getInstance());
+
+    sl.registerLazySingleton(
+      () => SecureStorage(
+        const FlutterSecureStorage(
+          aOptions: AndroidOptions(encryptedSharedPreferences: true),
+          iOptions: IOSOptions(accessibility: KeychainAccessibility.first_unlock),
+        ),
+      ),
+    );
+
+    sl.registerLazySingleton(() => DirectoryUtil());
+
+    sl.registerLazySingleton(() => HiveManager(sl(), sl()));
+
+    sl.registerLazySingleton(() => HiveEncryption(sl(), sl()));
+
+    sl.registerLazySingleton(() => HiveStorage<SessionModel>(sl(), sl()));
+
+    sl.registerLazySingleton(() => SessionManager(sl()));
+
+    sl.registerLazySingleton(() => DioClient(sl(), sl()));
+
+    sl.registerLazySingleton(() => AppConfig());
   }
 }
